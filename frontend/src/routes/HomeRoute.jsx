314805e1724ef16d209a33a4d619312b4e617ceb
Kingsley Eneja <kingsley.eneja@getir.com>
2023-09-16 14:30:00 +0100

    Implement HomeRoute

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import TopNavigation from 'components/TopNavigationBar';
+import PhotoList from 'components/PhotoList';
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => {
+const HomeRoute = (props) => {
   return (
-    <div className="home-route">
-      {/* Insert React */}
+    <div className='home-route'>
+      <TopNavigation topics={props.topics} />
+      <PhotoList photos={props.photos} />
     </div>
   );
 };
