7ca8694e4c9348c66be1cedfcafcf42259cce97e
Kingsley Eneja <kingsley.eneja@getir.com>
2023-09-17 16:01:50 +0100

    Close display modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
-import '../styles/PhotoDetailsModal.scss'
+import '../styles/PhotoDetailsModal.scss';
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = (props) => {
   return (
-    <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
+    <div className='photo-details-modal'>
+      <button
+        className='photo-details-modal__close-button'
+        onClick={() => props.setDisplayModal(false)}
+      >
+        <img src={closeSymbol} alt='close symbol' />
       </button>
     </div>
-  )
+  );
 };
 
 export default PhotoDetailsModal;
